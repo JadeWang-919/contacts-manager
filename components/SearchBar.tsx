2005a1ed013742cd66e33a37c002f180300ef2ce
JadeWang-919 <angelawangqi@hotmail.com>
2025-04-15 23:03:51 -0400

    improve colors

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -9,19 +9,19 @@ export default function SearchInput() {
   const setQuery = useSearchStore((state) => state.setQuery);
 
   return (
-    <div className="flex items-center gap-2 text-gray-400 bg-white dark:bg-zinc-800 ring-1 ring-gray-300 dark:ring-zinc-600 rounded-md px-3 py-2 focus-within:ring-2 focus-within:ring-emerald-200 transition w-64">
-      <IoSearch className="dark:text-zinc-600 text-lg" />
+    <div className="flex items-center gap-2 text-gray-400 dark:text-zinc-300 bg-white dark:bg-zinc-800 ring-1 ring-gray-300 dark:ring-zinc-600 rounded-md px-3 py-2 focus-within:ring-2 focus-within:ring-emerald-200 transition w-64">
+      <IoSearch className="text-lg" />
       <input
         type="text"
         placeholder="Search your contacts..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="flex-1 min-w-0 outline-none text-sm"
+        className="placeholder:text-gray-400 flex-1 min-w-0 outline-none text-sm"
       />
       {query && (
         <MdCancel
           onClick={() => setQuery("")}
-          className="text-lg cursor-pointer hover:text-gray-600 transition"
+          className="text-lg cursor-pointer hover:text-gray-500 dark:hover:text-zinc-400 transition"
         />
       )}
     </div>
